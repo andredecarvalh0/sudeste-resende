d3578b7653c2a99158d9e28937780fda70d4be80
and. Creative <71243944+andcreative@users.noreply.github.com>
2022-03-31 13:32:33 -0300

    Add files via upload

--- wp-content/themes/consultstreet/assets/js/custom.js
+++ wp-content/themes/consultstreet/assets/js/custom.js
@@ -10,20 +10,12 @@
             if (jQuery(window).scrollTop() >= 100) {
                 jQuery('.header-sticky').addClass('header-fixed-top');
 				jQuery('.header-sticky').removeClass('not-sticky');
-				jQuery('.navbar-collapse').removeClass('show');
             }
             else {
                 jQuery('.header-sticky').removeClass('header-fixed-top');
 				jQuery('.header-sticky').addClass('not-sticky');
             }
         });
-		
-		jQuery(".theme-main-slider").focusin(function(){
-			jQuery('.navbar-collapse').removeClass('show');
-		});
-		jQuery(".theme-page-header-area").focusin(function(){
-			jQuery('.navbar-collapse').removeClass('show');
-		});
 		
 		/* ---------------------------------------------- /*
          * Scroll top
@@ -42,7 +34,39 @@
             return false;
         });
 		
-	
+	// This JS added for the Toggle button to work with the focus element.
+		if (window.innerWidth < 992) {
+			
+			document.addEventListener('keydown', function(e) {
+			let isTabPressed = e.key === 'Tab' || e.keyCode === 9;
+				if (!isTabPressed) {
+					return;
+				}
+				
+			const  focusableElements =
+				'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
+			const modal = document.querySelector('.navbar.navbar-expand-lg'); // select the modal by it's id
+
+			const firstFocusableElement = modal.querySelectorAll(focusableElements)[1]; // get first element to be focused inside modal
+			const focusableContent = modal.querySelectorAll(focusableElements);
+			const lastFocusableElement = focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
+
+			  if (e.shiftKey) { // if shift key pressed for shift + tab combination
+				if (document.activeElement === firstFocusableElement) {
+				  lastFocusableElement.focus(); // add focus for the last focusable element
+				  e.preventDefault();
+				}
+			  } else { // if tab key is pressed
+				if (document.activeElement === lastFocusableElement) { // if focused has reached to last focusable element then focus first focusable element after pressing tab
+				  firstFocusableElement.focus(); // add focus for the first focusable element
+				  e.preventDefault();			  
+				}
+			  }
+
+			});
+		}
+		
+		
 		// Accodian Js
 		function toggleIcon(e) {
 			jQuery(e.target)
